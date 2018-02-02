69025c0c5a07aa9c9750940d92702a0e8960e206
Alisson R. Perez <alissonperez@gmail.com>
2018-02-02 17:25:03 -0200

    Add simple parser

---
+++ src/parsers/simple.test.js
@@ -0,0 +1,33 @@
+const simpleParser = require('./simple')
+
+test('simple parsing "a.b.c"', () => {
+    const expected = {
+	'a': {
+	    'b': {
+		'c': {}
+	    }
+	}
+    }
+
+    const val = 'a.b.c'
+    expect(simpleParser(val)).toEqual(expected)
+})
+
+test('simple parsing with "a.b.c,a.f,d.e.f"', () => {
+    const expected = {
+	'a': {
+	    'b': {
+		'c': {}
+	    },
+	    'f': {}
+	},
+	'd': {
+	    'e': {
+		'f': {}
+	    }
+	}
+    }
+
+    const val = 'a.b.c,a.f,d.e.f'
+    expect(simpleParser(val)).toEqual(expected)
+})
